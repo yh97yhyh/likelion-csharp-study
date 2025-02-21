b8269c4623fec18b7535fc998388093323a717f5
YoungHyun Kim <yh97yhyh@naver.com>
2025-02-21 10:23:13 +0900

    feat: test

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age; // 정수형 변수
+            age = 25; // 변수에 값 저장
+
+            Console.WriteLine(age); // 변수에 저장된 값을 출력
+        }
+    }
+}
