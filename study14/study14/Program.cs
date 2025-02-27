9dd0a541b6694ae0da1c135bf100b410f93563b1
YoungHyun Kim <yh97yhyh@naver.com>
2025-02-27 11:01:59 +0900

    day6 namespace

--- study14/study14/Program.cs
+++ study14/study14/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// namespace
+// 클래스, 함수, 변수 등 이름이 충돌하는 것을 방지하기 위해 사용
+namespace Dev1
+{
+    class MyClass
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("안녕하세요! dev1의 MyClass의 SayHello입니다");
+        }
+    }
+}
+
 namespace study14
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Dev1.MyClass.SayHello();
         }
     }
 }
