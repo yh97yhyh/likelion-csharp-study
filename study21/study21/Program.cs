299c4be942c0d084744f3d5d365682216f0b957d
YoungHyun Kim <yh97yhyh@naver.com>
2025-03-04 14:32:15 +0900

    day8

---
+++ study21/study21/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace study21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 컬렉션
+
+            // List
+            //var names = new List<string> { "Alice", "Bob", "Charlie" };
+
+            //names.Add("Dave");
+            //names.Remove("Bob");
+
+            //foreach (var name in names)
+            //{
+            //    Console.Write(name + " ");
+            //}
+
+            //Console.WriteLine();
+
+            //names.Insert(1, "Jolie");
+            //foreach (var name in names)
+            //{
+            //    Console.Write(name + " ");
+            //}
+
+            // Stack
+            //var stack = new Stack<int>();
+
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+
+            //while (stack.Count > 0)
+            //{
+            //    Console.Write($"{stack.Pop()} ");
+            //}
+
+            // Queue
+            //var queue = new Queue<int>();
+
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+
+            //while (queue.Count > 0)
+            //{
+            //    Console.Write($"{queue.Dequeue()} ");
+            //}
+
+            // ArrayList
+            //var arrayList = new ArrayList();
+
+            //arrayList.Add(1);
+            //arrayList.Add("Hello");
+            //arrayList.Add(3.14);
+
+            //foreach(var item in arrayList)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+            //Console.WriteLine();
+
+            //arrayList.Remove(1);
+
+            //foreach (var item in arrayList)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+            // Hashtable
+            //var hashtable = new Hashtable();
+
+            //hashtable["Alice"] = 25;
+            //hashtable["Bob"] = 30;
+            //hashtable["포션"] = 20;
+
+            //foreach(DictionaryEntry entry in hashtable)
+            //{
+            //    Console.WriteLine($"{entry.Key}: {entry.Value} ");
+            //}
+
+            //Console.WriteLine($"\nAlice의 나이: {hashtable["Alice"]}\n");
+
+            //hashtable.Remove("Bob");
+
+            //foreach (DictionaryEntry entry in hashtable)
+            //{
+            //    Console.WriteLine($"{entry.Key}: {entry.Value} ");
+            //}
+
+        }
+    }
+}
