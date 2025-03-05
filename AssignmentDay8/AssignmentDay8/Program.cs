bbac9bf97055d75c53fcd41e2e5049528231c002
YoungHyun Kim <yh97yhyh@naver.com>
2025-03-05 11:22:06 +0900

    day9

--- AssignmentDay8/AssignmentDay8/Program.cs
+++ AssignmentDay8/AssignmentDay8/Program.cs
@@ -32,68 +32,68 @@ namespace AssignmentDay8
         static void Main(string[] args)
         {
             // 과제 1
-            //var warrior = new Warrior("홍길동", 80, 96);
-            //Console.WriteLine("Name\tScore\tStrength");
-            //Console.WriteLine($"{warrior.Name}\t{warrior.Score}\t{warrior.Strength}");
+            var warrior = new Warrior("홍길동", 80, 96);
+            Console.WriteLine("Name\tScore\tStrength");
+            Console.WriteLine($"{warrior.Name}\t{warrior.Score}\t{warrior.Strength}");
 
             // 과제 2
-            //Console.Write("숫자를 입력하세요: ");
-            //try
-            //{
-            //    var input = Console.ReadLine();
-            //    var num = int.Parse(input);
-            //    Console.WriteLine($"{num}을 입력했습니다.");
-            //}
-            //catch (FormatException)
-            //{
-            //    Console.WriteLine("올바른 숫자를 입력하세요.");
-            //}
+            Console.Write("숫자를 입력하세요: ");
+            try
+            {
+                var input = Console.ReadLine();
+                var num = int.Parse(input);
+                Console.WriteLine($"{num}을 입력했습니다.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("올바른 숫자를 입력하세요.");
+            }
 
             // 과제3
-            //var fruits = new List<string>();
-            //fruits.Add("사과");
-            //fruits.Add("바나나");
-            //fruits.Add("포도");
+            var fruits = new List<string>();
+            fruits.Add("사과");
+            fruits.Add("바나나");
+            fruits.Add("포도");
 
-            //foreach(var fruit in fruits)
-            //{
-            //    Console.Write(fruit + " ");
-            //}
-            //Console.WriteLine();
+            foreach (var fruit in fruits)
+            {
+                Console.Write(fruit + " ");
+            }
+            Console.WriteLine();
 
-            //var queue = new Queue<string>();
-            //queue.Enqueue("첫번째작업");
-            //queue.Enqueue("두번째작업");
-            //queue.Enqueue("세번째작업");
+            var queue = new Queue<string>();
+            queue.Enqueue("첫번째작업");
+            queue.Enqueue("두번째작업");
+            queue.Enqueue("세번째작업");
 
-            //while(queue.Count > 0)
-            //{
-            //    Console.Write(queue.Dequeue() + " ");
-            //}
-            //Console.WriteLine();
+            while (queue.Count > 0)
+            {
+                Console.Write(queue.Dequeue() + " ");
+            }
+            Console.WriteLine();
 
-            //var stack = new Stack<int>();
-            //stack.Push(10);
-            //stack.Push(20);
-            //stack.Push(30);
+            var stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
 
-            //while (stack.Count > 0)
-            //{
-            //    Console.Write(stack.Pop() + " ");
-            //}
-            //Console.WriteLine();
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+            Console.WriteLine();
 
             // 과제4
-            //Console.Write("문장을 입력하세요: ");
-            //var input = Console.ReadLine();
+            Console.Write("문장을 입력하세요: ");
+            var input2 = Console.ReadLine();
 
-            //var upperInput = input.ToUpper();
-            //Console.WriteLine(upperInput);
+            var upperInput = input2.ToUpper();
+            Console.WriteLine(upperInput);
 
-            //var replacedInput = input.Replace("C#", "CSharp");
-            //Console.WriteLine(replacedInput);
+            var replacedInput = input2.Replace("C#", "CSharp");
+            Console.WriteLine(replacedInput);
 
-            //Console.WriteLine(input.Length);
+            Console.WriteLine(input2.Length);
 
             // 과제5
             var nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
---
+++ study23/study23/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambda
+            //var names = new List<string> { "Charlie", "Alice", "Bob" };
+            //var sortedNames = names.OrderBy(n => n);
+
+            //foreach (var name in sortedNames)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //var firstName = names.First(n => n.StartsWith("A"));
+
+            //Console.WriteLine($"First name starting with A: {firstName}");
+
+            //int[] nums = { 5, 3, 8, 1 };
+
+            ////메서드구문
+            //var sortedMeshod = nums.OrderByDescending(n => n);
+
+            ////쿼리 구문
+            //var sortedQuery = from n in nums
+            //                  orderby n
+            //                  select n;
+
+
+            //Console.WriteLine("Meshod syntax:");
+            //foreach (var n in sortedMeshod)
+            //    Console.Write(n + " ");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Query syntax:");
+            //foreach (var n in sortedQuery)
+            //{
+            //    Console.Write(n + " ");
+            //}
+
+            // Near 알고리즘
+
+            // Rank 알고리즘
+            var nums = new List<int> { 95, 90, 90, 80, 75 };
+            var ranked = Rank(nums);
+
+            foreach(var num in nums)
+            {
+                Console.WriteLine($"score: {num}, rank: {ranked[num]}");
+            }
+            Console.WriteLine();
+
+        }
+
+        static int FindNearest(List<int> nums, int target)
+        {
+            int left = 0, right = nums.Count - 1;
+            while (true)
+            {
+                if (left >= right)
+                {
+                    break;
+                }
+
+                int mid = (left + right) / 2;
+                if (nums[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+
+            if (left > 0 && Math.Abs(nums[left - 1] - target) < Math.Abs(nums[left] - target))
+                return nums[left - 1];
+            return nums[left];
+        }
+
+        // Rank
+        static Dictionary<int, int> Rank(List<int> nums)
+        {
+            var sorted = nums.Distinct().OrderByDescending(x => x).ToList(); // 중복 제거 후 내림차순
+
+            var dict = new Dictionary<int, int>(); // rank, score 
+
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                var num = sorted[i];
+                dict[num] = i + 1;
+            }
+
+            return dict;
+        }
+    }
+}
